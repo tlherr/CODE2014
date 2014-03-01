4e8b3d79439687afb2c0d064a1a67156a616840b
Thomas Herr <tom@renegadedigital.com>
2014-03-01 11:05:41 -0500

    Emberjs now able to actually pull data and persist.

--- src/Renegade/Bundle/CarImpactBundle/Resources/public/js/ember/app.js
+++ src/Renegade/Bundle/CarImpactBundle/Resources/public/js/ember/app.js
@@ -1,66 +1,63 @@
 $(document).ready(function() {
 
+
     window.App = Ember.Application.create();
+
     App.Store = DS.Store.extend({
         adapter: DS.FixtureAdapter
     });
 
-    App.ApplicationAdapter = DS.RESTAdapter.extend({
-        host: 'http://code2014.mark.renegade.local/app_dev.php',
-        namespace: 'api/v1'
+
+    //Routing
+
+    App.Router.map(function(){
+        this.route('contributors');
     });
 
-    //Create Models
-    App.Make = DS.Model.extend({
+
+    App.IndexRoute = Ember.Route.extend({
+        setupController: function(controller) {
+            // This sets the IndexController's title property:
+            controller.set('title', "The is the index controller");
+        }
+    });
+
+    App.ContributorsRoute = Ember.Route.extend({
+        model: function() {
+            return App.Contributor.all();
+        }
+    });
+
+    //Models
+
+    var attr = DS.attr;
+    App.Contributor = DS.Model.extend({
         label: attr(),
         canonical_label: attr(),
         name: attr()
     });
 
-    App.Model = DS.Model.extend({
-        make_id: attr(),
-        label: attr(),
-        year: attr(),
-        transmission: attr()
-    });
-
-    //Create a bunch of fake routes that map to the backend api calls for data
-    App.Router.map(function() {
-        this.route("make", { path: "/makes" });
-        this.route("model", { path: "/makes/:make_id/models" });
-    });
-
-
-    App.MakeRoute = Ember.Route.extend({
-        model: function() {
-            return this.store.find('makes');
+    App.Contributor.reopenClass({
+        allContributors: [],
+        all: function(){
+            this.allContributors = [];
+            $.ajax({
+                url: window.location.protocol + "//" + window.location.host + "/app_dev.php/api/v1/makes",
+                dataType: 'json',
+                context: this,
+                success: function(response){
+                    response.makes.forEach(function(contributor){
+                        this.allContributors.addObject(App.Contributor.createRecord(contributor))
+                    }, this)
+                }
+            });
+            return this.allContributors;
         }
     });
 
 
-    App.ModelRoute = Ember.Route.extend({
-        model: function(params) {
-            return this.store.find('model', params.make_id);
-        }
-    });
 
 
-    //Then use these routes to populate local datastores
-
-    var vehicleMake = function(query, cb) {
-        var results = $.map(['!', '!!', '!!!'], function(appendage) {
-            var datum = { make: query + appendage };
-
-            return datum;
-        });
-
-        cb(results);
-    };
-
-    $('#vehicle-make-typeahead').typeahead(null, {
-        displayKey: 'make',
-        source: vehicleMake
-    });
 
 });
 
