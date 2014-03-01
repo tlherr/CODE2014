1c7c094029ab759b41f6aee547255b1440d0fbf3
Mark Hayes <mark@renegadedigital.com>
2014-03-01 03:18:04 -0500

    Loading FOS Rest Bundle and JMS Serializer

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Renegade\Bundle\CarImpactBundle\RenegadeCarImpactBundle(),
+
+            // API
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
