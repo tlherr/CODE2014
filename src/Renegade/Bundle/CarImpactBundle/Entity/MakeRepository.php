7fd4ecdad3d745a58452c5ba1314beae1a5e8fc8
Mark Hayes <mark@renegadedigital.com>
2014-03-01 03:16:10 -0500

    Added entity repositories for make and model

---
+++ src/Renegade/Bundle/CarImpactBundle/Entity/MakeRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Renegade\Bundle\CarImpactBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Renegade\Bundle\CarImpactBundle\Helpers\StringHelpers;
+
+class MakeRepository extends EntityRepository {
+    /**
+     * @param string $filter
+     * @return \Doctrine\ORM\Query
+     */
+    public function getMakeQuery($filter = '')
+    {
+        $qb = $this->createQueryBuilder('m');
+
+        if (!empty($filter)) {
+            $filter = StringHelpers::getCanonical($filter);
+            $qb->where('m.canonicalLabel LIKE :filter')
+                ->setParameter('filter', sprintf('%%%s%%', $filter))
+            ;
+        }
+
+        return $qb->getQuery();
+    }
+} 
