d4ed7dcacfa198cdc77956b14753932858ff7f8a
Thomas Herr <tom@renegadedigital.com>
2014-03-01 02:50:50 -0500

    Updated models

--- src/Renegade/Bundle/CarImpactBundle/Resources/public/js/ember/app.js
+++ src/Renegade/Bundle/CarImpactBundle/Resources/public/js/ember/app.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
 
     window.App = Ember.Application.create();
-
     App.Store = DS.Store.extend({
         adapter: DS.FixtureAdapter
+    });
+
+    //Create Models
+    App.make = DS.Model.extend({
+        label: attr(),
+        canonical_label: attr(),
+        name: attr()
+    });
+
+    App.model = DS.Model.extend({
+        make_id: attr(),
+        label: attr(),
+        year: attr(),
+        transmission: attr()
     });
 
     //Create a bunch of fake routes that map to the backend api calls for data
 
     //Then use these routes to populate local datastores
-    
+
     var vehicleMake = function(query, cb) {
         var results = $.map(['!', '!!', '!!!'], function(appendage) {
             var datum = { make: query + appendage };
