83dc71fde5aa84c026a22e0ee0a0a5f824e942af
Mark Hayes <mark@renegadedigital.com>
2014-03-02 06:09:35 -0500

    Passing lint

--- src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/sorting.js
+++ src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/sorting.js
@@ -1,15 +1,13 @@
-
-
-$('.filter').click(function(e) {
-    if($(this).attr('data-sort-order') === 'none') {
-        $(this).attr('data-sort-order', 'desc');
-    } else if($(this).attr('data-sort-order') === 'desc') {
-        $(this).attr('data-sort-order', 'asc');
-    } else {
-        $(this).attr('data-sort-order', 'desc');
-    }
-
-    $('.filter').not($(this)).attr('data-sort-order', 'none');
-
-    $('.sortable').tsort({ attr: $(e.target).attr('data-sort-attr'), order: $(this).attr('data-sort-order')  });
-});
+(function ($) {
+    $('.filter').click(function(e) {
+        if($(this).attr('data-sort-order') === 'none') {
+            $(this).attr('data-sort-order', 'desc');
+        } else if($(this).attr('data-sort-order') === 'desc') {
+            $(this).attr('data-sort-order', 'asc');
+        } else {
+            $(this).attr('data-sort-order', 'desc');
+        }
+        $('.filter').not($(this)).attr('data-sort-order', 'none');
+        $('.sortable').tsort({ attr: $(e.target).attr('data-sort-attr'), order: $(this).attr('data-sort-order')  });
+    });
+})(jQuery);
