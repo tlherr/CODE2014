be2f9ef6d2c9c62b4565ffaa2a340ad2b1cd8338
Thomas Herr <tom@renegadedigital.com>
2014-03-01 23:44:45 -0500

    Added "select2" style form elements and functionality

--- src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/backbone/views/app-view.js
+++ src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/backbone/views/app-view.js
@@ -7,83 +7,36 @@ var app = app || {};
     app.AppView = Backbone.View.extend({
         el: '#vehicle-app',
         events: {
-            'blur #make-entry': 'doSomethingWhenFocused'
+            'blur #make-entry': 'loadModels'
         },
         initialize: function () {
             this.$list = this.$('#vehicle-list');
             this.$makeEntry = this.$('#make-entry');
             this.$modelEntry = this.$('#model-entry');
 
-            app.makesBloodhound = new Bloodhound({
-                datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.canonical_label); },
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                prefetch: {
-                    url: window.carImpactSettings.apiPath + 'makes'
-                },
-                limit: 10
-            });
-
-            app.modelsBloodhound = new Bloodhound({
-                datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.label); },
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                remote:  {
-                    url: 'http://code2014.mark.renegade.local/app_dev.php/api/v1/models',
-                    ajax: {
-                        data: {
-                            make: 748
-                        }
-                    }
-                }
-            });
-
-            app.makesBloodhound.initialize();
-            app.modelsBloodhound.initialize();
-
-            this.$makeEntry.typeahead(null, {
-                displayKey: 'label',
-                source: app.makesBloodhound.ttAdapter()
-            }).on('typeahead:selected', this.makeSelected);
-
-            this.$modelEntry.typeahead(null, {
-                displayKey: 'label',
-                valueKey: 'id',
-                source: app.modelsBloodhound.ttAdapter()
-            });
+            $("#make-entry").select2();
 
             this.listenTo(app.vehicles, 'add', this.addVehicle);
-
+            this.loadMakes();
             this.render();
         },
-        render: function () {
-
-        },
-        doSomethingWhenFocused: function () {
-            var that,
-                vID,
-                newVehicle;
-
-            that = this;
-
-            vID = this.$makeEntry.val();
-            if (undefined !== vID && '' !== vID) {
-                that.$makeEntry.val('');
-                // Don't bother retrieving the vehicle if we have it
-                if (undefined === app.vehicles.get(vID)) {
-                    newVehicle = new app.Vehicle({id: vID});
-                    newVehicle.fetch({
-                        success: function (obj) {
-                            app.vehicles.add(obj);
-                        }
-                    });
-                }
-            }
-        },
+        render: function () {},
         addVehicle: function (vehicle) {
             var vehicleView = new app.VehicleView({ model: vehicle });
             this.$list.append(vehicleView.render().el);
         },
-        makeSelected: function(e, object) {
-            app.modelsBloodhound.remote.ajax.data.make = object.id;
+        loadMakes: function() {
+            app.makes.fetch({
+                success: function(makes) {
+                    $.each(makes.models , function(index, value) {
+                        ($('<option>', { value: value.attributes.id }).text(value.attributes.label)).appendTo('#make-entry');
+                    });
+                }
+            });
+        },
+        loadModels: function() {
+            //Take the current value of the Makes form element
+            console.log((this.$makeEntry).val());
         }
     });
 })();
