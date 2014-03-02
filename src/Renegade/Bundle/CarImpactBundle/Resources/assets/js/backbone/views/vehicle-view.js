cde84bdbd5f7ff38de4e2768f13f151402dd476a
Thomas Herr <tom@renegadedigital.com>
2014-03-02 04:12:47 -0500

    Sorting now functioning on new data

--- src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/backbone/views/vehicle-view.js
+++ src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/backbone/views/vehicle-view.js
@@ -10,6 +10,20 @@ var app = app || {};
         render: function () {
             console.log(this.model);
             this.$el.html(this.template(this.model.toJSON()));
+
+            // data-highway-mpg="<%- mileage.highway.mpg %>" data-city-mpg="<%- mileage.city.mpg %>" data-transmission="<%- transmission %>" data-fuel="<%- fuel %>" data-year="<%- year %>" data-engine-size="<%- engine_size %>"
+            var mileage = this.model.get('mileage');
+            this.$el.addClass('sortable vehicle');
+
+            this.$el.attr('data-highway-mpg', mileage.highway.mpg);
+            this.$el.attr('data-highway-lph', mileage.highway.lph);
+            this.$el.attr('data-city-mpg', mileage.city.mpg);
+            this.$el.attr('data-city-lph', mileage.city.lph);
+
+            this.$el.attr('data-transmission', this.model.get('transmission'));
+            this.$el.attr('data-fuel', this.model.get('fuel'));
+            this.$el.attr('data-year', this.model.get('year'));
+            this.$el.attr('data-engine-size', this.model.get('engine-size'));
             return this;
         }
     });
