fa002ac72b09b0ab013efb14aeb5d1f2d294ccec
Thomas Herr <tom@renegadedigital.com>
2014-03-01 20:10:48 -0500

    Formatting cleanup on typeahead.js

--- src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/typeahead.js
+++ src/Renegade/Bundle/CarImpactBundle/Resources/assets/js/typeahead.js
@@ -1,29 +1,33 @@
+
 var makes = new Bloodhound({
-    datumTokenizer: function(d) {
-        console.log(d);
-        return Bloodhound.tokenizers.whitespace(d.label);
-    },
+    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.label); },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: 'http://code2014.mark.renegade.local/app_dev.php/api/v1/makes'
+});
+
+var models = new Bloodhound({
+    datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.label); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: 'test'
 });
 
 makes.initialize();
 
 $('#make-entry')
-    .typeahead(null, {
-        displayKey: 'label',
-        source: makes.ttAdapter()
-    }).on('typeahead:opened', function() {
-        console.log('opened');
-    }).on('typeahead:selected', function(e, object) {
-        console.log(arguments);
-        console.log('selected');
+.typeahead(null, {
+    displayKey: 'label',
+    source: makes.ttAdapter()
+}).on('typeahead:opened', function() {
+}).on('typeahead:selected', function(e, object) {
+    models.remote = 'http://code2014.mark.renegade.local/app_dev.php/api/v1/makes/' + object.id + '/models';
+    models.initialize();
+});
 
-        //Do next query with ID from first
 
-    }).on('typeahead:autocompleted', function($e, datum){
-        console.log('autocompleted');
-        console.log(datum);
-    });
+$('#model-entry')
+.typeahead(null, {
+    displayKey: 'label',
+    source: models.ttAdapter()
+});
 
 
